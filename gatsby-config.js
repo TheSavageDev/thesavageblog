283043113397c13827b5d8a798cab3e524d5681e
Jason A Savage <jason@thesavage.dev>
2021-01-05 18:30:32 -0600

    rss

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,8 @@ module.exports = {
   siteMetadata: {
     title: `The Savage Dev`,
     description: `Personal Site and Blog`,
-    author: `@json`
+    author: `@json`,
+    siteUrl: `https://thesavage.dev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -41,6 +42,62 @@ module.exports = {
         theme_color: `#333`,
         icon: `src/images/fev_icon.png` // This path is relative to the root of the site.
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allContentfulBlogs } }) => {
+              return allContentfulBlogs.edges.map(({ node }) => {
+                return {
+                  title: node.title,
+                  url: `${site.siteMetadata.siteUrl}/${node.slug}`,
+                  custom_elements: [
+                    { 'content:encoded': node.description.childMarkdownRemark.rawMarkdownBody },
+                    { cover_image: node.featureImage.fluid.src },
+                    { date: node.date },
+                  ],
+                }
+              })
+            },
+            query: `
+              {
+                allContentfulBlogs(sort: { fields: date, order: DESC }) {
+                  edges {
+                    node {
+                      title
+                      date
+                      slug
+                      featureImage {
+                        fluid(maxWidth: 1500) {
+                          src
+                        }
+                      }
+                      description {
+                        childMarkdownRemark {
+                          rawMarkdownBody
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: `/rss.xml`,
+            title: `The Savage Dev`,
+          },
+        ]
+      }
+    },
   ]
 };
